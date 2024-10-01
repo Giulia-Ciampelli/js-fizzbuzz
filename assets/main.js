2e85ecc03a0031e4c5c529703ed5c150d2220060
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-01 15:14:34 +0200

    docs: use modulus operator, fizz and buzz work

--- assets/main.js
+++ assets/main.js
@@ -3,13 +3,13 @@ console.log('i\'m alive');
 // for loop da 1 a 100, multipli di 3: stampa fizz; multipli di 5: buzz; multipli di entrambi: fizzbuzz
 
 for (let i = 1; i <= 100; i++) {
-    if (i == 3) {
+    if (i % 3 == 0) {
         console.log('Fizz');
     }
-    else if (i == 5) {
+    else if (i % 5 == 0) {
         console.log('Buzz');
     }
-    else if (i == 15) {
+    else if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('FizzBuzz');
     }
     else {
