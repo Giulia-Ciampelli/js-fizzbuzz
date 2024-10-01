6f82b0dc12e3922669489ef980a5061581445246
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-01 14:40:48 +0200

    docs: add test logs

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,15 @@
 console.log('i\'m alive');
 
-// for loop da 1 a 100, mmultipli di 3: stampa fizz; multipli di 5: buzz
+// for loop da 1 a 100, multipli di 3: stampa fizz; multipli di 5: buzz
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    if (i = i * 3) {
+        console.log('Fizz');
+    }
+    else if (i = i * 5) {
+        console.log('Buzz');
+    }
+    else {
+        console.log(i);
+    }    
 }
