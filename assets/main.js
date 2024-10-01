b36b7d6e9e5ac7cff7656803c0f85565f23c8809
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-01 15:52:29 +0200

    fix: strict inequality to only show fizzbuzz

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,10 @@ console.log('i\'m alive');
 // for loop da 1 a 100, multipli di 3: stampa fizz; multipli di 5: buzz; multipli di entrambi: fizzbuzz
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 !== 0) {
         console.log('Fizz');
     }
-    else if (i % 5 == 0) {
+    else if (i % 5 == 0 && i % 3 !== 0) {
         console.log('Buzz');
     }
     if (i % 3 == 0 && i % 5 == 0) {
